d3dfa309e553358db3e33c8db31961af5d48ea7d
Deeksha <136815400+Deekshakhathi@users.noreply.github.com>
2026-09-02 19:13:28 +0530

    Time: 3 ms (76.96%), Space: 46.2 MB (93.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,49 +1,40 @@
 class Solution {
-    public int romanToInt(String s) {
-        int ans=0;
-        for(int i=0;i<s.length();i++){
-              if(s.charAt(i) =='I'){
-                if(i+1<s.length() && s.charAt(i+1) == 'V'){
-                    ans += 4;
-                    i++;
-                }else if(i+1<s.length()  && s.charAt(i+1)=='X'){
-                    ans += 9;
-                    i++;
-                }else{
-                    ans += 1;
-                }
-              }else if(s.charAt(i) == 'X'){
-                if(i+1<s.length()  && s.charAt(i+1) == 'L'){
-                    ans+=40;
-                    i++;
-                }else if(i+1<s.length()  && s.charAt(i+1)=='C'){
-                    ans+=90;
-                    i++;
-                }else{
-                    ans+=10;
-                }
-              }else if(s.charAt(i)=='C'){
-                if(i+1<s.length()  && s.charAt(i+1) == 'D'){
-                    ans+=400;
-                    i++;
-                }else if(i+1<s.length()  && s.charAt(i+1)=='M'){
-                    ans+=900;
-                    i++;
-                }else{
-                    ans+=100;
-                }
-              }else if(s.charAt(i)=='V'){
-                ans += 5;
-              }else if(s.charAt(i)=='L'){
-                ans+=50;
-              }else if(s.charAt(i)=='D'){
-                ans+=500;
-            }else if(s.charAt(i)=='M'){
-                ans+=1000;
-            }else{
-                return -1;
-            }
+    public int romanToInt(String s){
+      int ans=0;
+      for(int i=0;i<s.length();i++){
+        int c=formula(s.charAt(i));
+        if(i<s.length()-1 && c<formula(s.charAt(i+1))){
+            ans-=c;
+        }else{
+            ans+=c;
         }
-        return ans;
+      }
+    return ans;
+      
     }
+      public int formula(char m){
+        if (m=='I'){ 
+            return 1;
+        }
+        if (m=='V'){
+             return 5;
+        } 
+        if (m=='X'){
+            return 10;
+        } 
+        if (m=='L') {
+            return 50;
+        }
+        if (m=='C') {
+            return 100;
+        }
+        if (m=='D'){
+            return 500;
+        }
+        if (m=='M' ){
+            return 1000;
+        }
+        return 0;
+      }
+    
 }
